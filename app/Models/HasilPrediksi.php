6669f7e81ee1ed09e6ea9baee808c39ae4ff46ec
farhanrbagask <farhanbagask@gmail.com>
2026-08-22 22:33:50 +0700

    Fix : HasilPrediksi.php

--- app/Models/HasilPrediksi.php
+++ app/Models/HasilPrediksi.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class HasilPrediksi extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'hasil_prediksi';
+    protected $primaryKey = 'id_prediksi';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id_prediksi',
+        'data_uji_id',
+        'akurasi',
+        'presisi',
+        'recall',
+        'f1_score'
+    ];
+
+    public function dataUji()
+    {
+        return $this->belongsTo(DataUji::class, 'data_uji_id', 'id_uji');
+    }
+
+    public function getHasilPrediksiAttribute()
+    {
+        return $this->dataUji ? $this->dataUji->hasil_prediksi : null;
+    }
+
+    public function getKelasAktualAttribute()
+    {
+        return $this->dataUji ? $this->dataUji->kelas : null;
+    }
 }
