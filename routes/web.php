e9555dc58c5eece30d5b0ee4857528e96f5781d0
farhanrbagask <farhanbagask@gmail.com>
2026-08-24 23:51:10 +0700

    Menambahkan routes baru

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,52 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DataLatihController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.process');
+
+Route::get('/register', [AuthController::class, 'showRegister'])
+    ->name('register');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register.process');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/data-latih', [DataLatihController::class, 'index'])
+        ->name('data-latih.index');
+
+    Route::get('/data-latih/create', [DataLatihController::class, 'create'])
+        ->name('data-latih.create');
+
+    Route::post('/data-latih', [DataLatihController::class, 'store'])
+        ->name('data-latih.store');
+
+    Route::get('/data-latih/{dataLatih}', [DataLatihController::class, 'show'])
+        ->name('data-latih.show');
+
+    Route::get('/data-latih/{dataLatih}/edit', [DataLatihController::class, 'edit'])
+        ->name('data-latih.edit');
+
+    Route::put('/data-latih/{dataLatih}', [DataLatihController::class, 'update'])
+        ->name('data-latih.update');
+
+    Route::delete('/data-latih/{dataLatih}', [DataLatihController::class, 'destroy'])
+        ->name('data-latih.destroy');
 });
